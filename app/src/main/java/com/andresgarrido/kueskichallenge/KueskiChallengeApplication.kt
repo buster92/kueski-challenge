a0fc1637ccc2f000e89e043e77d1ea790716d8bd
agreviver <andres.garrido@reviver.com>
2024-08-07 02:06:21 -0400

    added Application class

---
+++ app/src/main/java/com/andresgarrido/kueskichallenge/KueskiChallengeApplication.kt
@@ -0,0 +1,11 @@
+package com.andresgarrido.kueskichallenge
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * @author Andres Garrido on July 28, 2024
+ *         Copyright (C) 2024. All rights reserved.
+ */
+@HiltAndroidApp
+class KueskiChallengeApplication: Application()
